264fcf2987886371028f81f8d52c0fa62983db85
LAPTOP-SVEN\Sven <svenboeckx2@gmail.com>
2023-03-28 15:49:16 +0200

    Created Pause menu

--- Assets/Scripts/Events/GameEventsManager.cs
+++ Assets/Scripts/Events/GameEventsManager.cs
@@ -1,11 +1,17 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
 
 namespace RollaBall.Events
 {
     public class GameEventsManager : MonoBehaviour
     {
         public static GameEventsManager Instance { get; private set; }
+
+        [SerializeField] UnityEngine.InputSystem.InputActionAsset _inputActions = default;
+
+        private InputAction _actionCancel = default;
 
         private void Awake()
         {
@@ -14,6 +20,23 @@ namespace RollaBall.Events
                 Debug.LogError("Found more than one Game Events Manager in the scene.");
             }
             Instance = this;
+
+            _actionCancel = _inputActions.FindAction("UI/Cancel");
+            _actionCancel.Enable();
+        }
+
+        private void Update()
+        {
+            if (_actionCancel.WasPressedThisFrame())
+            {
+                Pause();
+            }
+        }
+
+        public static event Action OnPause = delegate { };
+        public static void Pause()
+        {
+            OnPause();
         }
 
         public static event Action OnPickUpCollected = delegate { };
@@ -27,5 +50,7 @@ namespace RollaBall.Events
         {
             OnWinGame();
         }
+
+
     }
 }
